c841bf3466bcc7fde4bbe01a7b336b47c5041710
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-18 22:23:16 -0400

    get sign name based on day of yr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
 
+//GLOBAL VARIABLES
+
+const DateTime = luxon.DateTime;
+
+const DUMMY_LEAP_YR = 2024; // ensures functionality for Feb 29 birthdays
+
+class Sign{
+    constructor(name, startDayOfLpYr, endDayOfLpYr){
+        this.name = name;
+        this.startDay = startDayOfLpYr;
+        this.endDay = endDayOfLpYr;
+    }
+}
+const SIGNS = [
+    new Sign('aries', 81, 110),
+    new Sign('taurus', 111, 141),
+    new Sign('gemini', 142, 172),
+    new Sign('cancer', 173, 204),
+    new Sign('leo', 205, 235),
+    new Sign('virgo', 236, 266),
+    new Sign('libra', 267, 296),
+    new Sign('scorpio', 297, 326),
+    new Sign('sagittarius', 327, 356),
+    new Sign('capricorn', 357, 19), //WILL REQ SPECIAL HANDLING
+    new Sign('aquarius', 20, 49),
+    new Sign('pisces', 50, 80)
+];
 
 
+//FUNCTIONS
+
+// Get horoscope based on sign name
 function getHoroscope(sign){
-
     fetch('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=' + sign + '&day=today',
         {
             method: 'POST',
@@ -29,6 +58,12 @@ function getHoroscope(sign){
 }
 
 
+// Get sign name based on day of yr
+function getSignName(dayOfYr){
+    var sign = SIGNS.find(sign => dayOfYr >= sign.startDay && dayOfYr <= sign.endDay);
 
-//TESTER CODE
-var horoscope = getHoroscope('sagittarius');
+    if (sign)
+        return sign.name;
+    else
+        return 'capricorn';
+}
