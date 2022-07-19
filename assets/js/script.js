2caf78a5cfa946efa46e9901c18d50461e900cd5
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-18 22:39:00 -0400

    redo logic for sign name based on day of yr

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,25 @@
 
 const DateTime = luxon.DateTime;
 
-const DUMMY_LEAP_YR = 2024; // ensures functionality for Feb 29 birthdays
-
 class Sign{
-    constructor(name, startDayOfLpYr, endDayOfLpYr){
+    constructor(name, lastDayOfLeapYr){
         this.name = name;
-        this.startDay = startDayOfLpYr;
-        this.endDay = endDayOfLpYr;
+        this.lastDay = lastDayOfLeapYr;
     }
 }
 const SIGNS = [
-    new Sign('aries', 81, 110),
-    new Sign('taurus', 111, 141),
-    new Sign('gemini', 142, 172),
-    new Sign('cancer', 173, 204),
-    new Sign('leo', 205, 235),
-    new Sign('virgo', 236, 266),
-    new Sign('libra', 267, 296),
-    new Sign('scorpio', 297, 326),
-    new Sign('sagittarius', 327, 356),
-    new Sign('capricorn', 357, 19), //WILL REQ SPECIAL HANDLING
-    new Sign('aquarius', 20, 49),
-    new Sign('pisces', 50, 80)
+    new Sign('capricorn', 19),
+    new Sign('aquarius', 49),
+    new Sign('pisces', 80),
+    new Sign('aries', 110),
+    new Sign('taurus', 141),
+    new Sign('gemini', 172),
+    new Sign('cancer', 204),
+    new Sign('leo', 235),
+    new Sign('virgo', 266),
+    new Sign('libra', 296),
+    new Sign('scorpio', 326),
+    new Sign('sagittarius', 356)
 ];
 
 
@@ -60,7 +57,7 @@ function getHoroscope(sign){
 
 // Get sign name based on day of yr
 function getSignName(dayOfYr){
-    var sign = SIGNS.find(sign => dayOfYr >= sign.startDay && dayOfYr <= sign.endDay);
+    var sign = SIGNS.find(elem => dayOfYr <= elem.lastDay)
 
     if (sign)
         return sign.name;
