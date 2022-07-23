704fe2a33a61287740f654a7c46d093a580bc101
claudiayile <claudiayile@Gmail.com>
2022-07-23 19:31:23 -0400

    for loop generating unique query strings from keyword array

---
+++ assets/js/script.js
@@ -0,0 +1,178 @@
+//GLOBAL VARIABLES
+
+const DateTime = luxon.DateTime;
+
+const DUMMY_LEAP_YR = 2024; // assume that all birthdays occur in a leap year (to ensure Feb 29 functionality)
+
+class Sign{
+    constructor(name, lastDayOfLeapYr){
+        this.name = name;
+        this.lastDay = lastDayOfLeapYr;
+    }
+}
+const SIGNS = [
+    new Sign('capricorn', 19),
+    new Sign('aquarius', 49),
+    new Sign('pisces', 80),
+    new Sign('aries', 110),
+    new Sign('taurus', 141),
+    new Sign('gemini', 172),
+    new Sign('cancer', 204),
+    new Sign('leo', 235),
+    new Sign('virgo', 266),
+    new Sign('libra', 296),
+    new Sign('scorpio', 326),
+    new Sign('sagittarius', 356)
+];
+
+const monthSelectorEl = $('select[name="month"]');
+const daySelectorEl = $('select[name="day"]');
+
+
+
+//FUNCTIONS
+
+// Populate user input 'days' dropdown with correct # of days for given month
+function setNumDays(month){
+    var firstOfThisMonth = DateTime.fromFormat(`${month} 1 ${DUMMY_LEAP_YR}`, 'MMMM d y');
+    var firstOfNextMonth = firstOfThisMonth.plus({months: 1});
+    var daysInMonth = firstOfNextMonth.diff(firstOfThisMonth, 'days').toObject().days;
+
+    var daySelected = daySelectorEl.val() || 1;
+    
+    daySelectorEl.empty();
+    for (i = 1; i <= daysInMonth; i++)
+        daySelectorEl.append($(`<option value='${i}'>${i}</option>`));
+
+    if (daySelected <= daysInMonth)
+        daySelectorEl.val(daySelected);
+}
+
+
+// Get horoscope based on sign name
+function getHoroscope(month, day){
+    month = "November";
+    day = "12";
+    var signName = getSignName(month, day);
+
+    fetch('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=' + signName + '&day=today',
+        {
+            method: 'POST',
+            headers: {
+                'X-RapidAPI-Key': '0998422ae9msh631f094298f7caep1b0a7bjsnf6c1671b7893',
+                'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+            }
+        }
+    )
+        .then(response => response.json())
+        .then(data => {
+            var horoscopeObj = { 
+                color: data.color,
+                desc: data.description,
+                luckyNum: data.lucky_number,
+                mood: data.mood
+            };
+            extractFromText(horoscopeObj,"topics");
+//             extractFromText(horoscopeObj,"feelings");
+        })
+        .catch(error =>
+            console.log('system error') //UPDATE LATER with something that the user can actually see (a modal?)
+        );
+}
+
+
+// Get sign name based on month and day
+function getSignName(month, day){
+    var dayOfYr = +DateTime.fromFormat(`${month} ${day} ${DUMMY_LEAP_YR}`, 'MMMM d y').toFormat('o');
+
+    var sign = SIGNS.find(elem => dayOfYr <= elem.lastDay);
+
+    if (sign)
+        return sign.name;
+    else
+        return 'capricorn';
+}
+
+// Get key feelings or topics given text input, extractType options = ["topics","feelings"]
+function extractFromText(horoscopeObj, extractType) {
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "db93cfc0d2mshb30b8e666594cd2p1659b8jsn866b6f92afba",
+      "X-RapidAPI-Host": "textprobe.p.rapidapi.com",
+    },
+    body: '{"text":"' + horoscopeObj.desc + '"}',
+  };
+
+  fetch("https://textprobe.p.rapidapi.com/" + extractType, options)
+    .then((response) => response.json())
+    .then((data) => {
+      if(extractType=="topics"){
+        var keywords= data.keywords;
+        keywords.push(horoscopeObj.color);
+        keywords.push(horoscopeObj.luckyNum);
+        keywords.push(horoscopeObj.mood);
+        console.log(keywords);
+        return keywords;
+      }
+      else if (extractType=="feelings"){
+        var feelings = [data.emotion_prediction];
+        feelings.push(horoscopeObj.mood);
+        console.log(feelings);
+        return feelings;
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("q", keywords);
+encodedParams.append("accessToken", "ef149044f1msh6c4ae87fc053ebbp1a9994jsnaeca69b4627c");
+
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': '00173b333cmsh7f497d732d65894p1a0c45jsn8fad42dfb52d',
+		'X-RapidAPI-Host': 'Spotifystefan-skliarovV1.p.rapidapi.com'
+	},
+	body: encodedParams
+};
+
+fetch('https://spotifystefan-skliarovv1.p.rapidapi.com/search', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+//LISTENERS
+
+//Update # of days when month is (re-)selected
+monthSelectorEl.on('change', function(event){
+    event.preventDefault();
+    setNumDays($(this).val());
+});
+
+
+//Upon birthday submission, begin the chain of API calls
+$('#birthday-input').on('submit', function(event){
+    event.preventDefault();
+    getHoroscope(monthSelectorEl.val(), daySelectorEl.val());
+});
+
+
+
+//INITIALIZE PAGE
+monthSelectorEl.val(DateTime.now().toFormat('MMMM').toLowerCase()); // set initial month to today's
+monthSelectorEl.trigger('change'); // initialize day dropdown w/ correct # of days for the initial month
+daySelectorEl.val(+DateTime.now().toFormat('d')); // set initial day-of-month to today's
+
+  // separate keywords into searchable strings
+
+  // search spotify with keywords from generator 
+ 
+
+
+
+  
